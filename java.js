e4051b67e65b40fe5069beb746f38efcb7e946b5
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2021-12-30 15:48:52 +0300

    logo

--- java.js
+++ java.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     
 })
-// $(document).ready(function() {
-//     $('.Wedo').hide();
-//     $('Wedo').animate({
-//         opacity:1
-//     });
-//     $('Wedo').hover(function() {
-//         $(this).stop().animate({opacity:.4},200);
-//         $('.Wedo').fadeIn();
-//     }, function() {
-//         $(this).stop().animate({opacity:1},500)
-//         $('.Wedo').fadeOut();
-//     });
-// });
+$(document).ready(function() {
+    $('.Wedo').hover();
+    $('Wedo').animate({
+        opacity:1
+    });
+    $('Wedo').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.Wedo').fadeIn();
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.Wedo').fadeOut();
+    });
+});
