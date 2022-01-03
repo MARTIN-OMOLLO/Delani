7c162c22fa93367e51c9b74815052d87ae2fb875
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-01-03 16:00:48 +0300

    email

--- java.js
+++ java.js
@@ -1,34 +1,56 @@
 // $(document).ready(function(){
 
-$(document).ready(function() {
-    $(".Development").click(function(){
+$(document).ready(function () {
+    $(".Development").click(function () {
         $(".imgtwo").toggle();
         $(".paratwo").toggle();
     });
-    })
-    $(document).ready(function() {
-        $(".Design").click(function(){
-            $(".imgone").toggle();
-            $(".paraone").toggle();
-        });
+})
+$(document).ready(function () {
+    $(".Design").click(function () {
+        $(".imgone").toggle();
+        $(".paraone").toggle();
     });
-    $(document).ready(function() {
-        $(".Management").click(function(){
-            $(".imgthree").toggle();
-            $(".parathree").toggle();
-        });
+});
+$(document).ready(function () {
+    $(".Management").click(function () {
+        $(".imgthree").toggle();
+        $(".parathree").toggle();
     });
-    $(document).ready(function(){
-        $(".col").click(function(){
-            $(".col").hover()
-            element.onmouseover = function() {
-                //Hovering
-              }
-        })
+});
+$(document).ready(function () {
+    $(".col").click(function () {
+        $(".col").hover()
+        element.onmouseover = function () {
+            //Hovering
+        }
     })
-  function sendMessage (){
-     var name = document.getElementById("name").value; 
-     var email = document.getElementById("email").value;
-     var message = document.getElementById("message").value;
-     var inputedName = validInput(); validEmail();  validMessage(); validName();
-  }
+})
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var inputedName = validName(); validEmail(); validMessage();
+
+    function validName() {
+        if (name == "") {
+            alert("Enter name");
+            return false;
+        }
+    }
+    function validEmail(){
+        if (email == "") {
+            alert("Enter Email");
+            return false;
+        }
+
+    }
+    function validMessage(){
+        if (message == "") {
+            alert("Write a message");
+            return true;
+    
+        }
+    }
+    alert("Thank you " + document.getElementById("name").value + ",We got your message");
+};
